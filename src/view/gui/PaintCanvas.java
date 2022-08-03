a382756db709c97ea8a1e374abab8484852785a0
colinfindley <62814733+colinfindley@users.noreply.github.com>
2022-08-03 02:14:06 -0500

    glitch fix

--- src/view/gui/PaintCanvas.java
+++ src/view/gui/PaintCanvas.java
@@ -1,12 +1,9 @@
 package view.gui;
 
-<<<<<<< Updated upstream
-import model.DrawShape;
-=======
 import model.SelectedShapeList;
 import model.ShapeList;
 import model.interfaces.IShape;
->>>>>>> Stashed changes
+
 import view.interfaces.PaintCanvasBase;
 
 import java.awt.*;
@@ -16,14 +13,10 @@ public class PaintCanvas extends PaintCanvasBase {
     @Override
     public void paint(Graphics g) {
         Graphics2D graphics2d = (Graphics2D) g;
-<<<<<<< Updated upstream
-        DrawShape.draw(graphics2d);
-=======
         for (IShape shape : ShapeList.shapeList)
             shape.draw(graphics2d);
         for (IShape shape : SelectedShapeList.selectedShapeList)
             shape.selectDraw(graphics2d);
->>>>>>> Stashed changes
     }
 
     public Graphics2D getGraphics2D() {
