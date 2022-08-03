d034fac8adc70d55d3921df3ff367322efdc9154
colinfindley <62814733+colinfindley@users.noreply.github.com>
2022-08-03 18:47:44 -0500

    Sprint 3 Finished

--- src/model/shapes/Shape.java
+++ src/model/shapes/Shape.java
@@ -9,40 +9,11 @@ import java.awt.*;
 abstract public class Shape implements IShape {
 
     public ShapeConfiguration shape;
-    private int x;
-    private int y;
-    private int width;
-    private int height;
-    private Point firstPoint;
-    private Point lastPoint;
-    private int x1;
-    private int x2;
-    private int y1;
-    private int y2;
-    private int[] xPoints;
-    private int[] yPoints;
-
 
     public ShapeConfiguration getShapeConfig() {
         return this.shape;
     }
 
     public void selectDraw(Graphics2D g) {}
-
-    public Graphics2D setOutline(Graphics2D g) {
-        return g;
-    }
-
-    public Graphics2D setOutlineAndFilledIn(Graphics2D g) {
-        return g;
-    }
-
-    public Graphics2D setSelect(Graphics2D g) {
-        g.setStroke(new BasicStroke(5));
-        g.setColor(shape.shapeColorPrimary);
-        return g;
-    }
-
-
 
 }
