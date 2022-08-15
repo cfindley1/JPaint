b2de0e86f9751b67723590a3e6012cbe3c0191eb
colinfindley <62814733+colinfindley@users.noreply.github.com>
2022-08-14 21:45:34 -0500

    Buggy undo/redo

--- src/model/commands/GroupShapeCommand.java
+++ src/model/commands/GroupShapeCommand.java
@@ -11,12 +11,15 @@ import model.shapes.ShapeGroup;
 import view.gui.PaintCanvas;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class GroupShapeCommand implements IUndoable, ICommand {
 
     // Fields
     ShapeGroup shapeGroupReference;
+    List<ShapeGroup> groupList = new ArrayList<ShapeGroup>();
+
     PaintCanvas paintCanvas = PaintCanvas.getInstance();
 
     // Constructor
@@ -27,25 +30,40 @@ public class GroupShapeCommand implements IUndoable, ICommand {
     public void execute() {
         ShapeGroupConfiguration groupedShapes = new ShapeGroupConfiguration();
         for (IShape shape : SelectedShapeList.selectedShapeList) {
+            System.out.println(shape);
             groupedShapes.add(shape);
+            groupList.add(shape.getGroup());
         }
         ShapeGroup shapeGroup = new ShapeGroup(groupedShapes);
         shapeGroupReference = shapeGroup;
-        for (IShape shape : groupedShapes.getList())
+        for (IShape shape : groupedShapes.getList()) {
+            System.out.println(shape);
             shape.setGroup(shapeGroup);
+        }
         ShapeList.add(shapeGroup);
         paintCanvas.repaint();
         CommandHistory.add(this);
+        System.out.println("execute done");
     }
 
     @Override
     public void undo() {
+        for (ShapeGroup group : groupList) {
+            for (IShape shape : shapeGroupReference.getShapeConfig().getList()) {
+                shape.setGroup(group);
+            }
+        }
         ShapeList.remove(shapeGroupReference);
         paintCanvas.repaint();
     }
 
     @Override
     public void redo() {
+        for (ShapeGroup group : groupList) {
+            for (IShape shape : shapeGroupReference.getShapeConfig().getList()) {
+                shape.setGroup(group);
+            }
+        }
         ShapeList.add(shapeGroupReference);
         paintCanvas.repaint();
     }
